6342f54b2a783598540852bac8025643dee80924
sofiamgsfernandes <sofiamgsfernandes@gmail.com>
2017-10-06 15:11:58 +0100

    lla

---
+++ graphs/graph_struct.py
@@ -0,0 +1,29 @@
+class Vertex:
+    def __init__(self, id_vertex, weight):
+        self.id = id_vertex
+        self.neighbors = []
+        self.weight=weight
+        
+    def add_neighbor(self, neighbor):
+        if isinstance(neighbor, Vertex):
+            if neighbor.id not in self.neighbors:
+                self.neighbors.append(neighbor.id)
+                neighbor.neighbors.append(self.id)
+                self.neighbors = sorted(self.neighbors, key=weight)
+                neighbor.neighbors = sorted(neighbor.neighbors, key=weight)
+        else:
+            return False
+        
+    def add_neighbors(self, neighbors):
+        for neighbor in neighbors:
+            if isinstance(neighbor, Vertex):
+                if neighbor.id not in self.neighbors:
+                    self.neighbors.append(neighbor.id)
+                    neighbor.neighbors.append(self.id)
+                    self.neighbors = sorted(self.neighbors, key=weight)
+                    neighbor.neighbors = sorted(neighbor.neighbors, key=weight)
+            else:
+                return False
+        
+    def __repr__(self):
+        return str(self.neighbors)
