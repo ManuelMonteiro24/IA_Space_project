74d3d6ed04d181495e9ae2b6a7c89a85be595826
ManuelMonteiro24 <tic-manuel13b@hotmail.com>
2017-10-26 11:05:18 +0100

    manl

--- uninformed_search_function.py
+++ uninformed_search_function.py
@@ -16,12 +16,12 @@ def general_search(problem, strategy, launches):
 
         if problem.goal_test(node):
             return node
-        explored.add(node.modules_in_space)
+        explored.add(frozenset(node.modules_in_space))
 
         successors = problem.find_successor(launches, node)
 
         for child_node in successors.values():
-            explored.add(child_node.modules_in_space)
+            explored.add(frozenset(child_node.modules_in_space))
 
             for node in strategy.queue:
                 if node.modules_in_space == child_node.modules_in_space and child_node.path_cost <= node.path_cost :
