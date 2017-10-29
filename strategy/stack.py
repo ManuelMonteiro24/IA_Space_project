b251e828e60a0b2bcf4852baca19809da1eb8815
ManuelMonteiro24 <tic-manuel13b@hotmail.com>
2017-10-29 10:41:40 +0000

    as

--- strategy/stack.py
+++ strategy/stack.py
@@ -1,5 +1,5 @@
+from graphs.graph_struct import *
 
-#strategy struct for Dfs or iterative Dfs
 class Stack:
 
     def __init__(self):
@@ -10,3 +10,29 @@ class Stack:
 
     def remove(self):
         return self.lifo.pop()
+
+class MyPriorityQueue():
+
+    def __init__(self):
+        self.list = list()
+        self.list_size = 0
+
+    def add_node(self, new_node):
+        if self.list_size == 0:
+            self.list.append(new_node)
+        else:
+            for node in self.list:
+                if new_node <= node:
+                    self.list.insert(0,new_node)
+                    self.list_size = self.list_size + 1
+                    break
+    def search_node(self, new_node):
+        if self.list_size == 0:
+            return False
+        else:
+            for node in self.list:
+                if new_node.modules_in_space == node.modules_in_space:
+                        if new_node < node:
+                            self.list.remove(node)
+                            self.add_node(new_node)
+                        break
