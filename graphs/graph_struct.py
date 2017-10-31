6ac0c5165bf32b71fde21278eea9eb34fa4552d3
ManuelMonteiro24 <tic-manuel13b@hotmail.com>
2017-10-31 23:02:46 +0000

    falta comentar o problem

--- graphs/graph_struct.py
+++ graphs/graph_struct.py
@@ -1,21 +1,23 @@
 import itertools
 
 class Vertex:
+    """Class that represents a vertex of the graph used in the Problem. Each vertex is represented by the Module ID and contains its associated weight and a list of neighbors which represents the vertexes that it is connected to."""
     def __init__(self, id_vertex, weight = -1):
         self.id = id_vertex
         self.neighbors = []
         self.weight=weight
 
     def add_neighbor(self, neighbor):
+        """Method that creates a connection between two vertexes by adding each one to the other neighbors list."""
         if isinstance(neighbor, Vertex):
             if neighbor.id not in self.neighbors:
                 self.neighbors.append(neighbor.id)
                 neighbor.neighbors.append(self.id)
-
         else:
             return False
 
 class Graph:
+    """Class that represents the graph used in the Problem. It has a dictionary indexed by the vertex id (Module ID) that contains all the vertexes in the graph."""
     def __init__(self):
         self.vertices = {}
         self.num_vertices = 0
@@ -23,19 +25,23 @@ class Graph:
 
 
     def add_vertex(self, vertex_id, vertex_weight = -1):
+        """Method that receives the data of a module and checks if it is already in the graph, if it isn't the vertex is created and added to the graph. If the vertex is already in the graph but doesn't has a weigth associated the weigth is added to the vertex."""
         if self.get_vertex(vertex_id) is None:
             self.num_vertices += 1
             if vertex_weight == -1:
+                #information about the vertex has not received only its connections
                 new_vertex = Vertex(vertex_id)
             else:
                 new_vertex = Vertex(vertex_id, vertex_weight)
             self.vertices[vertex_id] = new_vertex
             return new_vertex
         if self.vertices[vertex_id].weight == -1:
+            #vertex already in graph but doesn't have weigth associated
             self.vertices[vertex_id].weight = vertex_weight
 
 
     def get_vertex(self, vertex_id):
+        """Method that receives a vertex id (Module name) and checks if it is on the graph"""
         if vertex_id in self.vertices:
             return self.vertices[vertex_id]
         else:
@@ -43,9 +49,13 @@ class Graph:
 
 
     def add_edge(self, vertex_1_id, vertex_2_id):
+        """Method that receives the module id's from two modules and connects them both in the graph"""
+
         if self.get_vertex(vertex_1_id) == None:
+            #the first module isn't already on the graph we have to add it first
             self.add_vertex(vertex_1_id)
         if self.get_vertex(vertex_2_id) == None:
+            #the second module isn't already on the graph we have to add it first
             self.add_vertex(vertex_2_id)
 
         self.vertices[vertex_1_id].add_neighbor(self.vertices[vertex_2_id])
@@ -53,6 +63,7 @@ class Graph:
 
 
     def find_path(self, start_vertex, end_vertex, path = []):
+        """Method that tries to find a path in the graph between a the start and the end vertexes received in the arguments. This is simply done through a recursive search through the neighbors as long as we advance in the path. Is no path is found None is returned."""
         path += [start_vertex]
         if start_vertex == end_vertex:
             return path
@@ -68,7 +79,7 @@ class Graph:
         return None
 
     def __str__(self):
-        """ Function to print a graph as adjacency list and adjacency matrix. """
+        """Function to print a graph as adjacency list."""
         if len(self.vertices) >= 1:
                 return [str(key) + ", " + str(self.vertices[key].weight) + " :" + str(self.vertices[key]) for key in self.vertices.keys()]
         else:
@@ -77,6 +88,9 @@ class Graph:
 
 
 class Node():
+    """Class that represents the node type that is utilized in the problem. Each node contains: launch_id, that represents the associated launch;
+    launch_payload, with the modules that are carried to space; weight, that represents the total weight of modules to launched; path_cost; modules_in_space, a set with the modules already in space;
+    launch_cost, that represents the total cost of the launch with the modules in it; ancestor, that represents the parent node from which this node has expanded, in case of the first node this one is None."""
     def __init__(self):
         self.launch_id = 0
         self.launch_payload = -1
@@ -84,7 +98,6 @@ class Node():
         self.path_cost = 0
         self.modules_in_space = set()
         self.launch_cost = 0
-        self.launched = set()
         self.ancestor = None
 
     def __lt__(self, other):
